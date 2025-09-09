6b8faf5840a302a3d01f5fdb5cecf0750ce2c8e6
Hallan duhke <hallan.duhke@leaf.com.br>
2025-09-09 01:33:14 -0300

    End of section 2

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuctionService.Entities;
+
+[Table("Items")]
+public class Item
+{
+    [Key]
+    public  Guid Guid { get; set; }
+
+    public string Make { get; set; }
+
+    public string Model { get; set; }
+
+    public int Year { get; set; }
+
+    public string Color { get; set; }
+
+    public int Mileage { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    // nav properties
+
+    public Auction Auction { get; set; }
+
+    public Guid AuctionId { get; set; }
+}
